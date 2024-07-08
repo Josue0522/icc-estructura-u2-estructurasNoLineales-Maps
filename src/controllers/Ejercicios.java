3bc554f7167ccba17a8e4a1b135d750d434ae6c7
Josue <abadjosue05@gmail.com>
2024-07-08 17:51:13 -0500

    Ejercicios

--- src/controllers/Ejercicios.java
+++ src/controllers/Ejercicios.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Ejercicios {
 
@@ -10,26 +11,48 @@ public class Ejercicios {
      * cantidad,
      * sin importar el orden.
      *
-     * Ejemplo 1:
+     * * Ejemplo 1:
      * Input: str1 = "listen", str2 = "silent"
      * Output: true
      * Explicación: Ambas cadenas tienen los mismos caracteres con la misma
      * frecuencia.
-     *
+     * 
      * Ejemplo 2:
      * Input: str1 = "hello", str2 = "bello"
      * Output: false
      * Explicación: Las cadenas tienen diferentes caracteres.
-     *
+    
      * Ejemplo 3:
      * Input: str1 = "triangle", str2 = "integral"
      * Output: true
      * Explicación: Ambas cadenas tienen los mismos caracteres con la misma
      * frecuencia.
      */
+    
     public static boolean areAnagrams(String str1, String str2) {
-        throw new UnsupportedOperationException("Not implemented yet");
 
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // Si las longitudes de las cadenas son diferentes, no pueden ser anagramas
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Crear mapas de frecuencia para ambas cadenas
+        Map<Character, Integer> mapaFrecuencia1 = construirMapaFrecuencia(str1);
+        Map<Character, Integer> mapaFrecuencia2 = construirMapaFrecuencia(str2);
+
+        // Comparar los mapas de frecuencia
+        return mapaFrecuencia1.equals(mapaFrecuencia2);
+    }
+
+    private static Map<Character, Integer> construirMapaFrecuencia(String str) {
+        Map<Character, Integer> mapaFrecuencia = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            mapaFrecuencia.put(c, mapaFrecuencia.getOrDefault(c, 0) + 1);
+        }
+        return mapaFrecuencia;
     }
 
     /*
@@ -47,7 +70,9 @@ public class Ejercicios {
      * Input: nums = [9,2,3,6], objetivo = 10
      * Output: null
      */
-    public int[] sumatoriaDeDos(int[] nums, int objetivo) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+
+    //public int[] sumatoriaDeDos(int[] nums, int objetivo) {
+       // return sumatoriaDeDos(nums, objetivo);
+    //}
 }
+
